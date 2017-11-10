568f99916dd6f1465de299ef463f37012b9e37d2
Atticus Gurley <atticus.gurley@gmail.com>
2017-11-09 19:59:51 -0500

    Worked with navigation and footer, added other pages

--- js/app.js
+++ js/app.js
@@ -1,51 +1,53 @@
 //Loop through all songs for audio player
-// $(window).scroll(function (event) {
-//   $('.page').slideUp();
-// });
 
-function smoothScroll(event) {
+/******************************
+FUNCTION DECLARATIONS
+******************************/
 
-  // Make sure this.hash has a value before overriding default behavior
-  if (this.hash !== "") {
-
-    // Prevent default anchor click behavior
-    event.preventDefault();
-
-    // Store hash
-    var hash = this.hash;
-
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 800, function() {
-
-    // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
-    });
-
-  } // End if
-
+function smoothScroll(hash) {
+  // Using jQuery's animate() method to add smooth page scroll
+  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 800, function() {
+  // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+  });
 }
 
-// Add smooth scrolling on all links inside the navbar
-// $('a').on('click', smoothScroll);
+function scrollTrigger(event) {
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+    // Store hash
+    const hash = this.hash;
+    smoothScroll(hash);
+  } // End if
+}
 
-// On page load
+function fadeTrigger(event) {
+  if (this.hash !== "") {
+    // Prevent default anchor click behavior
+    event.preventDefault();
+    // Store hash
+    const hash = this.hash;
+    // FadeOut landing and fadeIn rest of site (like entering a portal)
+    $('#landing').fadeOut(function () {
+      $('.inside').fadeIn(function () {
+        smoothScroll(hash);
+      }); //End fadeIn
+    }); //End fadeOut
+  } // End if
+}
+
+/******************************
+LANDING
+******************************/
+
+// On page load default to only landing page displayed and remove hash value to reflect this
 window.location.hash = '';
 $('.inside').hide();
 $('#landing').show();
-
-$('h4 a').click(function (event) {
-  const hash = this.hash;
-  event.preventDefault();
-  $('#landing').fadeOut(function () {
-    $('.inside').fadeIn(function () {
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function() {
-        window.location.hash = hash;
-      });
-    });
-  });
-});
+// On link click, fade to rest of page and scroll to <div id="hash">
+$('h4 a').click(fadeTrigger);
